f0efdd645814fdde150b85cec26a75d8d027ed76
adithya1107 <adigaadithya70@gmail.com>
2025-01-09 18:40:20 +0530

    footer change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import HeroSection from './components/HeroSection.jsx'
 import About from './components/About.jsx'
-import Footer from './components/footer.jsx'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
   return (
